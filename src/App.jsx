66fb8ebe40aa7bbf76c11ced93c68ef8669fd162
Irfan Aziz Al Amin <irfan.aziz@mamiteam.com>
2024-04-09 15:44:18 +0700

    implement add job

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,35 @@ import MainLayout from "./layout/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/404";
 import JobPage, { jobLoader } from "./pages/JobPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      {/* "*" is to catch all other routes */}
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const response = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    const data = await response.json();
+    console.log(data);
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        {/* "*" is to catch all other routes */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   // we can only return a single component, so if there's more than one, add () "parenthesis" and wrap in single root
   // we can use <></> to be a wrapper
   return <RouterProvider router={router} />;
