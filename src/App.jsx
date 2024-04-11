44ad76b59f4f5f50d16eab89abe9dbf3d4100be3
Irfan Aziz Al Amin <irfan.aziz@mamiteam.com>
2024-04-11 09:03:08 +0700

    implement edit page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/404";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
   const addJob = async (newJob) => {
@@ -38,12 +39,30 @@ const App = () => {
     return;
   };
 
+  const updateJob = async (editedJob) => {
+    const response = await fetch(`/api/jobs/${editedJob.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editedJob),
+    });
+    const data = await response.json();
+    console.log(data);
+    return; 
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route
           path="/jobs/:id"
           element={<JobPage deleteJob={deleteJob} />}
